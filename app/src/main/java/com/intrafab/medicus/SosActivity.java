8d377d7cb917d05fd0ebe8392f1d17e50c277670
Artemiy Terekhov <terekhov@divo-games.com>
2015-06-02 15:40:16 +0300

    Complete layout sos screen

--- app/src/main/java/com/intrafab/medicus/SosActivity.java
+++ app/src/main/java/com/intrafab/medicus/SosActivity.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.view.ViewCompat;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 
 /**
  * Created by Artemiy Terekhov on 10.04.2015.
@@ -15,6 +17,9 @@ public class SosActivity extends BaseActivity {
     public static final String TAG = SosActivity.class.getName();
     public static final String EXTRA_OPEN_SOS = "openSos";
 
+    private ImageView mIconPhone;
+    private ImageView mIconHeadset;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,8 +28,15 @@ public class SosActivity extends BaseActivity {
 
         getSupportActionBar().setTitle("SOS");
         showActionBar();
+        setActionBarIcon(R.mipmap.ic_action_back);
 
         ViewCompat.setTransitionName(toolbar, EXTRA_OPEN_SOS);
+
+        mIconPhone = (ImageView) findViewById(R.id.ivPhone);
+        mIconHeadset = (ImageView) findViewById(R.id.ivHeadset);
+
+//        mIconPhone.setColorFilter(getResources().getColor(R.color.colorLightWindowBackground));
+        mIconHeadset.setColorFilter(getResources().getColor(R.color.colorLightWindowBackground));
     }
 
     @Override
@@ -41,5 +53,17 @@ public class SosActivity extends BaseActivity {
                         EXTRA_OPEN_SOS
                 );
         ActivityCompat.startActivity(activity, intent, options.toBundle());
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
