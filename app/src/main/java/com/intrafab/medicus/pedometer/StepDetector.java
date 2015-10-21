7187e5ae25749578a91468d3d24132da281398a1
Artemiy Terekhov <artemiy.terekhov@gmail.com>
2015-10-21 09:12:29 +0300

    Fix step detector algorithm

--- app/src/main/java/com/intrafab/medicus/pedometer/StepDetector.java
+++ app/src/main/java/com/intrafab/medicus/pedometer/StepDetector.java
@@ -3,6 +3,7 @@ package com.intrafab.medicus.pedometer;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 
 import com.intrafab.medicus.utils.Logger;
 
@@ -15,25 +16,39 @@ import java.util.ArrayList;
 public class StepDetector implements SensorEventListener {
     protected static final String TAG = StepDetector.class.getName();
 
-    protected static final float THRESHOLD_AMPLITUDE = 1.0f;
-    protected static final int THRESHOLD_INACTIVITY = 10;
-    //protected static final int SENSOR_RATE = 60000; //SensorManager.SENSOR_DELAY_UI;
-    public static final int MOVEMENT_NONE = 0;
-    public static final int MOVEMENT_FORWARD = 1;
-    public static final int MOVEMENT_BACKWARD = 2;
+//    protected static final float THRESHOLD_AMPLITUDE = 1.0f;
+//    protected static final int THRESHOLD_INACTIVITY = 10;
+//    //protected static final int SENSOR_RATE = 60000; //SensorManager.SENSOR_DELAY_UI;
+//    public static final int MOVEMENT_NONE = 0;
+//    public static final int MOVEMENT_FORWARD = 1;
+//    public static final int MOVEMENT_BACKWARD = 2;
 
     protected ArrayList<NotifyListener> mListeners;
-    private float mLastZ;
-    private int mLastActivity = MOVEMENT_NONE;
-    private int mInactivityCount = 0;
+//    private float mLastZ;
+//    private int mLastActivity = MOVEMENT_NONE;
+//    private int mInactivityCount = 0;
+//
+//    private SimpleFilter[] mFiltersCascade = new SimpleFilter[3];
 
-    private SimpleFilter[] mFiltersCascade = new SimpleFilter[3];
+    private float   mLimit = 10;
+    private float   mLastValues[] = new float[3*2];
+    private float   mScale[] = new float[2];
+    private float   mYOffset;
+
+    private float   mLastDirections[] = new float[3*2];
+    private float   mLastExtremes[][] = { new float[3*2], new float[3*2] };
+    private float   mLastDiff[] = new float[3*2];
+    private int     mLastMatch = -1;
 
     public StepDetector() {
         mListeners = new ArrayList<>();
-        mFiltersCascade[0] = new SimpleFilter(1, 1, 0.01f, 0.0025f);
-        mFiltersCascade[1] = new SimpleFilter(1, 1, 0.01f, 0.0025f);
-        mFiltersCascade[2] = new SimpleFilter(1, 1, 0.01f, 0.0025f);
+//        mFiltersCascade[0] = new SimpleFilter(1, 1, 0.01f, 0.0025f);
+//        mFiltersCascade[1] = new SimpleFilter(1, 1, 0.01f, 0.0025f);
+//        mFiltersCascade[2] = new SimpleFilter(1, 1, 0.01f, 0.0025f);
+
+        mYOffset = 480 * 0.5f;
+        mScale[0] = - (480 * 0.5f * (1.0f / (SensorManager.STANDARD_GRAVITY * 2)));
+        mScale[1] = - (480 * 0.5f * (1.0f / (SensorManager.MAGNETIC_FIELD_EARTH_MAX)));
     }
 
     public void addListener(NotifyListener listener) {
@@ -46,28 +61,72 @@ public class StepDetector implements SensorEventListener {
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        if (event.sensor.getType() != Sensor.TYPE_ACCELEROMETER) {
-            return;
-        }
-        final float z = filter(event.values[2]);
-        if (Math.abs(z - mLastZ) > THRESHOLD_AMPLITUDE) {
-            mInactivityCount = 0;
-            int currentActivity = (z > mLastZ) ? MOVEMENT_FORWARD : MOVEMENT_BACKWARD;
-            if (currentActivity != mLastActivity) {
-                mLastActivity = currentActivity;
-                notifyListeners(currentActivity);
+//        if (event.sensor.getType() != Sensor.TYPE_ACCELEROMETER) {
+//            return;
+//        }
+//        final float z = filter(event.values[2]);
+//        if (Math.abs(z - mLastZ) > THRESHOLD_AMPLITUDE) {
+//            mInactivityCount = 0;
+//            int currentActivity = (z > mLastZ) ? MOVEMENT_FORWARD : MOVEMENT_BACKWARD;
+//            if (currentActivity != mLastActivity) {
+//                mLastActivity = currentActivity;
+//                notifyListeners(currentActivity);
+//            }
+//        } else {
+//            if (mInactivityCount > THRESHOLD_INACTIVITY) {
+//                if (mLastActivity != MOVEMENT_NONE) {
+//                    mLastActivity = MOVEMENT_NONE;
+//                    notifyListeners(MOVEMENT_NONE);
+//                }
+//            } else {
+//                mInactivityCount++;
+//            }
+//        }
+//        mLastZ = z;
+        Sensor sensor = event.sensor;
+        synchronized (this) {
+            if (sensor.getType() == Sensor.TYPE_ORIENTATION) {
             }
-        } else {
-            if (mInactivityCount > THRESHOLD_INACTIVITY) {
-                if (mLastActivity != MOVEMENT_NONE) {
-                    mLastActivity = MOVEMENT_NONE;
-                    notifyListeners(MOVEMENT_NONE);
+            else {
+                int j = (sensor.getType() == Sensor.TYPE_ACCELEROMETER) ? 1 : 0;
+                if (j == 1) {
+                    float vSum = 0;
+                    for (int i=0 ; i<3 ; i++) {
+                        final float v = mYOffset + event.values[i] * mScale[j];
+                        vSum += v;
+                    }
+                    int k = 0;
+                    float v = vSum / 3;
+
+                    float direction = (v > mLastValues[k] ? 1 : (v < mLastValues[k] ? -1 : 0));
+                    if (direction == - mLastDirections[k]) {
+                        // Direction changed
+                        int extType = (direction > 0 ? 0 : 1); // minumum or maximum?
+                        mLastExtremes[extType][k] = mLastValues[k];
+                        float diff = Math.abs(mLastExtremes[extType][k] - mLastExtremes[1 - extType][k]);
+
+                        if (diff > mLimit) {
+
+                            boolean isAlmostAsLargeAsPrevious = diff > (mLastDiff[k]*2/3);
+                            boolean isPreviousLargeEnough = mLastDiff[k] > (diff/3);
+                            boolean isNotContra = (mLastMatch != 1 - extType);
+
+                            if (isAlmostAsLargeAsPrevious && isPreviousLargeEnough && isNotContra) {
+                                Logger.i(TAG, "step");
+                                notifyListeners(0);
+                                mLastMatch = extType;
+                            }
+                            else {
+                                mLastMatch = -1;
+                            }
+                        }
+                        mLastDiff[k] = diff;
+                    }
+                    mLastDirections[k] = direction;
+                    mLastValues[k] = v;
                 }
-            } else {
-                mInactivityCount++;
             }
         }
-        mLastZ = z;
     }
 
     @Override
@@ -75,22 +134,22 @@ public class StepDetector implements SensorEventListener {
 
     }
 
-    /**
-     * Smoothes the signal from accelerometer
-     */
-    private float filter(float measurement) {
-        float f1 = mFiltersCascade[0].correct(measurement);
-        float f2 = mFiltersCascade[1].correct(f1);
-        float f3 = mFiltersCascade[2].correct(f2);
-        return f3;
-    }
+//    /**
+//     * Smoothes the signal from accelerometer
+//     */
+//    private float filter(float measurement) {
+//        float f1 = mFiltersCascade[0].correct(measurement);
+//        float f2 = mFiltersCascade[1].correct(f1);
+//        float f3 = mFiltersCascade[2].correct(f2);
+//        return f3;
+//    }
 
     /**
      * Calls registered event listeners
      */
     private void notifyListeners(int activity) {
-        if (activity == MOVEMENT_NONE)
-            return;
+//        if (activity == MOVEMENT_NONE)
+//            return;
 
         for (NotifyListener listener : mListeners) {
             listener.onStep(activity);
